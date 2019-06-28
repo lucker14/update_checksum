9f1091c630db044d8569eb37381db5fe71683376
Petr Pluhar <lucker14@gmail.com>
2019-06-28 08:52:30 +0200

    rm sth

--- lib/update_checksum.rb
+++ lib/update_checksum.rb
@@ -13,24 +13,8 @@ module UpdateChecksum
     text = File.read('.gitlab-ci.yml')
     new_contents = text.gsub(/GEMFILE_LOCK_HASH: '(.*?)'/, "GEMFILE_LOCK_HASH: '#{new_hash}'")
 
-    puts new_contents
-
     # To write changes to the file, use:
     File.open('.gitlab-ci.yml', 'w') {|file| file.puts new_contents }
   end
 
 end
-
-# Bundler::Plugin.add_hook('after-install-all') do
-#   puts 'Gems installed and loaded! Lets update Gemfile.lock hash for CI'
-
-#   new_hash = Digest::MD5.hexdigest(File.read('Gemfile.lock'))
-
-#   text = File.read('.gitlab-ci.yml')
-#   new_contents = text.gsub(/GEMFILE_LOCK_HASH: '(.*?)'/, "GEMFILE_LOCK_HASH: '#{new_hash}'")
-
-#   puts new_contents
-
-#   # To write changes to the file, use:
-#   File.open('.gitlab-ci.yml', 'w') {|file| file.puts new_contents }
-# end
